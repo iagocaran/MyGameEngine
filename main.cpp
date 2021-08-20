9f7d97929657084d0e056e457d03d2ae5e5b2a33
Iago <iago.caran@gmail.com>
2021-08-21 00:19:51 +0200

    Create first window and basic loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description) {
+    std::cout << "Error: " << description << std::endl;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        std::cout << "Error initializing glfw." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "GLFW Window", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Error opening Window." << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
+
